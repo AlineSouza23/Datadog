f902832c2350168e3593f3f4b1191a36e0fb9bb4
Aline Souza <aline2308souza@gmail.com>
2025-12-08 21:51:40 -0300

    sua mensagem aqui

--- src/App.js
+++ src/App.js
@@ -2,82 +2,55 @@ import React, { useState } from "react";
 
 const logPaths = {
   Autorização: "service:autorizacao-kafka-parser-mensageria AVRO env:prd",
-  Liquidação: "em desenvolvimento",
+  Liquidação: "service:liquidacao-kafka-parser-mensageria env:prd ",
   Standin: "service:standin-consumidor-autorizacao env:prd",
   Advice: "service:standin-consumidor-advice \"TRANSACAO PERSISTIDA COM SUCESSO \" env:prd ",
 };
 
 const campos = {
 Autorização: [
-  { grupo: "BIT22", itens: ["@Bit22capacidadePIN", "@Bit22codigoModoEntrada", "@Bit22modoEntrada"] },
-  { grupo: "BIT43", itens: ["@Bit43Estabelecimento", "@Bit43codigoPais", "@Bit43Cidade", "@Bit43Pais"] },
+  // -------------------------
+  // BITS ORDENADOS
+  // -------------------------
 
-  { grupo: "BIT46", itens: [
-    "@Bit46respostaAVS","@Bit46resultadoCVE2","@Bit46fonteAutorizacao","@Bit46floorLimit","@Bit46campoErro",
-    "@Bit46mudancaEndereco","@Bit46resultadoCVE","@Bit46resultadoCriptograma","@Bit46resultadoTVR","@Bit46resultadoCVR",
-    "@Bit46nomePortador","@Bit46numeroCobranca","@Bit46nomeCobranca","@Bit46cidadeCobranca","@Bit46cobrancaUF",
-    "@Bit46emailCobranca","@Bit46telefoneCobranca1","@Bit46telefoneCobranca2","@Bit46telefoneCobranca3",
-    "@Bit46padraoEnderecoCobranca","@Bit46respostaCAVV"
-  ]},
-
-  { grupo: "BIT54", itens: ["@Bit54tipoConta","@Bit54tipoValor","@Bit54codigoMoeda","@Bit54indicativoPlataforma","@Bit54valor"] },
-
-  { grupo: "BIT58", itens: ["@Bit58enderecoRua","@Bit58cep","@Bit58codigoPais","@Bit58numeroLoja","@Bit58nomeShopping"] },
-
-  { grupo: "BIT60", itens: [
-    "@Bit60tipoTerminal","@Bit60AprovacaoParcial","@Bit60localizacaoTerminal","@Bit60presencaPortador",
-    "@Bit60codigoPresencaCartao","@Bit60capacidadeCapturaCartao","@Bit60codigoStatusTransacao",
-    "@Bit60segurancaTransacao","@Bit60reservadoZerado","@Bit60codigoTipoPOS","@Bit60capacidadeEntrada",
-    "@Bit60condicaoEspecial","@Bit60reservadoZerado2","@Bit60presencaCartao","@Bit60statusTransacao","@Bit60tipoPOS"
-  ]},
-
-  { grupo: "BIT122", itens: [
-    "@Bit122tipoAutenticacao1","@Bit122formatoTLV","@Bit122tipoAutenticacao2",
-    "@Bit122codigoResultadoAutenticacao3DS","@Bit122segundoFatorAutenticacao","@Bit122chaveCAVV",
-    "@Bit122valorCAVV","@Bit122numeroImprevisivel","@Bit122rastreamentoAutenticacao","@Bit122versaoAcao",
-    "@Bit122acaoAutenticacao","@Bit122enderecoIP","@Bit122tipoAutenticacao3","@Bit122atc",
-    "@Bit122indicadorChaveCAVV","@Bit122outputCVE2","@Bit122numeroImprevisivelInApp",
-    "@Bit122rastreamentoAutenticacaoInapp","@Bit122versaoAcaoInapp","@Bit122acaoAutenticacaoInapp",
-    "@Bit122RUF","@Bit122tipoAutenticacao4","@Bit122atcTokenizada","@Bit122indicadorChaveTAVV",
-    "@Bit122TAVV","@Bit122numeroImprevisivelTokenizada","@Bit122rastreamentoAutenticacaoTokenizada",
-    "@Bit122RUFtokenizada"
-  ]},
-
-  { grupo: "BIT124", itens: [
-    "@Bit124condicaoDadosTransacao","@Bit124produtoCartao","@Bit124indicadorChipParcial","@Bit124indicadorRemocao",
-    "@Bit124indicadorHostCapture","@Bit124scoreCredenciador","@Bit124scoreELO","@Bit124reservadoZerado",
-    "@Bit124motivoPrimarioFraude","@Bit124motivoSecundarioFraude","@Bit124motivoTerciarioFraude","@Bit124decisaoFraude",
-    "@Bit124origemScore","@Bit124recomendacaoListaRestritiva","@Bit124recomendacaoindiceConfianca",
-    "@Bit124recomendacaoRegraAtaque","@Bit124recomendacaoMonitoriaAtiva","@Bit124reservadoZerado2",
-    "@Bit124indicadorAprovacaoJogos","@Bit124indicadorContaComprometida","@Bit124dataComprometimento",
-    "@Bit124totalViolacoesConfirmadas","@Bit124tipoComprometimento","@Bit124identificadorComprometimento"
-  ]},
-
-  // REMAPPERS
   { grupo: "BIT02", itens: ["@Bit02pan"] },
 
   { grupo: "BIT03", itens: [
-    "@Bit03codigoProcesso","@Bit03codigoContaOrigem","@Bit03contaOrigem","@Bit03tipoContaDestino","@Bit03processo"
+    "@Bit03codigoProcesso","@Bit03codigoContaOrigem","@Bit03contaOrigem",
+    "@Bit03tipoContaDestino","@Bit03processo"
   ]},
 
   { grupo: "BIT04", itens: ["@Bit04","@Bit04valorTransacao"] },
+
   { grupo: "BIT05", itens: ["@Bit05valorMoedaLiquidacao"] },
+
   { grupo: "BIT06", itens: ["@Bit06valorFaturaPortador"] },
+
   { grupo: "BIT07", itens: ["@Bit07dataHoraTransmissao"] },
+
   { grupo: "BIT08", itens: ["@Bit08taxaFaturaPortador"] },
+
   { grupo: "BIT09", itens: ["@Bit09conversaoMoedaLiquidacao"] },
+
   { grupo: "BIT10", itens: ["@Bit10conversaoFaturaPortador"] },
+
   { grupo: "BIT11", itens: ["@Bit11stan"] },
 
   { grupo: "BIT13_12", itens: ["@bit1213dataHoraTransacao"] },
 
   { grupo: "BIT14", itens: ["@Bit14ValidadeCartao"] },
+
   { grupo: "BIT15", itens: ["@Bit15dataLiquidacao"] },
+
   { grupo: "BIT16", itens: ["@Bit16dataConversaoMoeda"] },
 
   { grupo: "BIT18", itens: ["@Bit18codigoMCC","@Bit18MCC"] },
 
   { grupo: "BIT19", itens: ["@Bit19codigoPais"] },
+
+  { grupo: "BIT22", itens: [
+    "@Bit22capacidadePIN","@Bit22codigoModoEntrada","@Bit22modoEntrada"
+  ]},
 
   { grupo: "BIT23", itens: ["@Bit23sequenciaCartao"] },
 
@@ -98,13 +71,16 @@ Autorização: [
   { grupo: "BIT33", itens: ["@Bit33instituicaoRepasse"] },
 
   { grupo: "BIT35", itens: ["@Bit35trilha2Cartao"] },
+
   { grupo: "BIT36", itens: ["@Bit36trilha3Cartao"] },
 
   { grupo: "BIT37", itens: ["@Bit37nsu"] },
 
   { grupo: "BIT38", itens: ["@Bit38codigoAutorizacao"] },
 
-  { grupo: "BIT39", itens: ["@Bit39classificacaoCodigoResposta","@Bit39codigoResposta","@Bit39resposta"] },
+  { grupo: "BIT39", itens: [
+    "@Bit39classificacaoCodigoResposta","@Bit39codigoResposta","@Bit39resposta"
+  ]},
 
   { grupo: "BIT40", itens: ["@Bit40"] },
 
@@ -112,9 +88,23 @@ Autorização: [
 
   { grupo: "BIT42", itens: ["@Bit42mid"] },
 
+  { grupo: "BIT43", itens: [
+    "@Bit43Estabelecimento","@Bit43codigoPais","@Bit43Cidade","@Bit43Pais"
+  ]},
+
+  { grupo: "BIT46", itens: [
+    "@Bit46respostaAVS","@Bit46resultadoCVE2","@Bit46fonteAutorizacao","@Bit46floorLimit",
+    "@Bit46campoErro","@Bit46mudancaEndereco","@Bit46resultadoCVE","@Bit46resultadoCriptograma",
+    "@Bit46resultadoTVR","@Bit46resultadoCVR","@Bit46nomePortador","@Bit46numeroCobranca",
+    "@Bit46nomeCobranca","@Bit46cidadeCobranca","@Bit46cobrancaUF","@Bit46emailCobranca",
+    "@Bit46telefoneCobranca1","@Bit46telefoneCobranca2","@Bit46telefoneCobranca3",
+    "@Bit46padraoEnderecoCobranca","@Bit46respostaCAVV"
+  ]},
+
   { grupo: "BIT47", itens: [
-    "@Bit47identificacaoPedido","@Bit47parcelasAmortizacao","@Bit47parcelasCarencia","@Bit47periodicidadeAmortizacao",
-    "@Bit47periodicidadeCarencia","@Bit47taxaJurosAmortizacao","@Bit47taxaJurosCarencia","@Bit47transacaoTokenizada"
+    "@Bit47identificacaoPedido","@Bit47parcelasAmortizacao","@Bit47parcelasCarencia",
+    "@Bit47periodicidadeAmortizacao","@Bit47periodicidadeCarencia","@Bit47taxaJurosAmortizacao",
+    "@Bit47taxaJurosCarencia","@Bit47transacaoTokenizada"
   ]},
 
   { grupo: "BIT48", itens: [
@@ -129,16 +119,32 @@ Autorização: [
 
   { grupo: "BIT53", itens: ["@Bit53controleSeguranca"] },
 
+  { grupo: "BIT54", itens: [
+    "@Bit54tipoConta","@Bit54tipoValor","@Bit54codigoMoeda","@Bit54indicativoPlataforma","@Bit54valor"
+  ]},
+
   { grupo: "BIT56", itens: ["@Bit56dadosPortador"] },
 
+  { grupo: "BIT58", itens: [
+    "@Bit58enderecoRua","@Bit58cep","@Bit58codigoPais","@Bit58numeroLoja","@Bit58nomeShopping"
+  ]},
+
   { grupo: "BIT59", itens: ["@Bit59dadosTransporte"] },
+
+  { grupo: "BIT60", itens: [
+    "@Bit60tipoTerminal","@Bit60AprovacaoParcial","@Bit60localizacaoTerminal","@Bit60presencaPortador",
+    "@Bit60codigoPresencaCartao","@Bit60capacidadeCapturaCartao","@Bit60codigoStatusTransacao",
+    "@Bit60segurancaTransacao","@Bit60reservadoZerado","@Bit60codigoTipoPOS","@Bit60capacidadeEntrada",
+    "@Bit60condicaoEspecial","@Bit60reservadoZerado2","@Bit60presencaCartao",
+    "@Bit60statusTransacao","@Bit60tipoPOS"
+  ]},
 
   { grupo: "BIT61", itens: ["@Bit61"] },
 
   { grupo: "BIT62", itens: [
-    "@Bit62dadosTransacaoOnline", "@Bit62codigoAutenticacao", "@Bit62cnpjRaizComprador",
-    "@Bit62cnpjFilialComprador", "@Bit62cnpjDigitoComprador", "@Bit62cpfComprador",
-    "@Bit62cpfDigitoComprador", "@Bit62idComercioEletronico", "@Bit62eci"
+    "@Bit62dadosTransacaoOnline","@Bit62codigoAutenticacao","@Bit62cnpjRaizComprador",
+    "@Bit62cnpjFilialComprador","@Bit62cnpjDigitoComprador","@Bit62cpfComprador",
+    "@Bit62cpfDigitoComprador","@Bit62idComercioEletronico","@Bit62eci"
   ]},
 
   { grupo: "BIT63", itens: ["@Bit63"] },
@@ -193,7 +199,30 @@ Autorização: [
 
   { grupo: "BIT121", itens: ["@Bit121"] },
 
+  { grupo: "BIT122", itens: [
+    "@Bit122tipoAutenticacao1","@Bit122formatoTLV","@Bit122tipoAutenticacao2",
+    "@Bit122codigoResultadoAutenticacao3DS","@Bit122segundoFatorAutenticacao","@Bit122chaveCAVV",
+    "@Bit122valorCAVV","@Bit122numeroImprevisivel","@Bit122rastreamentoAutenticacao",
+    "@Bit122versaoAcao","@Bit122acaoAutenticacao","@Bit122enderecoIP","@Bit122tipoAutenticacao3",
+    "@Bit122atc","@Bit122indicadorChaveCAVV","@Bit122outputCVE2","@Bit122numeroImprevisivelInApp",
+    "@Bit122rastreamentoAutenticacaoInapp","@Bit122versaoAcaoInapp","@Bit122acaoAutenticacaoInapp",
+    "@Bit122RUF","@Bit122tipoAutenticacao4","@Bit122atcTokenizada","@Bit122indicadorChaveTAVV",
+    "@Bit122TAVV","@Bit122numeroImprevisivelTokenizada","@Bit122rastreamentoAutenticacaoTokenizada",
+    "@Bit122RUFtokenizada"
+  ]},
+
   { grupo: "BIT123", itens: ["@Bit123"] },
+
+  { grupo: "BIT124", itens: [
+    "@Bit124condicaoDadosTransacao","@Bit124produtoCartao","@Bit124indicadorChipParcial",
+    "@Bit124indicadorRemocao","@Bit124indicadorHostCapture","@Bit124scoreCredenciador",
+    "@Bit124scoreELO","@Bit124reservadoZerado","@Bit124motivoPrimarioFraude","@Bit124motivoSecundarioFraude",
+    "@Bit124motivoTerciarioFraude","@Bit124decisaoFraude","@Bit124origemScore",
+    "@Bit124recomendacaoListaRestritiva","@Bit124recomendacaoindiceConfianca",
+    "@Bit124recomendacaoRegraAtaque","@Bit124recomendacaoMonitoriaAtiva","@Bit124reservadoZerado2",
+    "@Bit124indicadorAprovacaoJogos","@Bit124indicadorContaComprometida","@Bit124dataComprometimento",
+    "@Bit124totalViolacoesConfirmadas","@Bit124tipoComprometimento","@Bit124identificadorComprometimento"
+  ]},
 
   { grupo: "BIT125", itens: ["@Bit125"] },
 
@@ -201,14 +230,18 @@ Autorização: [
 
   { grupo: "BIT127", itens: ["@Bit127versaoMensageria"] },
 
-  // CAMPOS ADICIONAIS
+  // --------------------------------
+  // GRUPOS NÃO-BIT (mantidos como estavam)
+  // --------------------------------
+
   { grupo: "dadosAdicionaisMensagem", itens: [
     "@codigoBandeira","@codigoCredenciadora","@Credenciador","@codigoEmissor","@Emissor",
     "@codigoMensagemComplementar","@maquinaStratus","@tipoSituacaoTransacao","@tipoTransacao",
-    "@origemInformacao","@indicadorCapturador","@indicadorToken","@bin","@finalCartao","@cartaoCriptografado",
-    "@hashCartaoCriptografado","@indicadorCartaoDigitalCaixa","@servicosEmv.codigoServicoARQC",
-    "@servicosEmv.indicadorValidacaoARQC","@nrid","@indicadorTokenizacaoCpfBit48",
-    "@indicadorTokenizacaoCnpjBit48","@indicadorTokenizacaoCpfBit104","@indicadorTokenizacaoCnpjBit104",
+    "@origemInformacao","@indicadorCapturador","@indicadorToken","@bin","@finalCartao",
+    "@cartaoCriptografado","@hashCartaoCriptografado","@indicadorCartaoDigitalCaixa",
+    "@servicosEmv.codigoServicoARQC","@servicosEmv.indicadorValidacaoARQC","@nrid",
+    "@indicadorTokenizacaoCpfBit48","@indicadorTokenizacaoCnpjBit48",
+    "@indicadorTokenizacaoCpfBit104","@indicadorTokenizacaoCnpjBit104",
     "@indicadorTokenizacaoRazaoSocial","@indicadorTransacaoAfe","@transacaoInternacional",
     "@indicadorBinTamanhoSeis","@tipoFinanciamentoTransacao","@idAutorizacaoTransacao",
     "@idChaveHsm","@razaoSocialEstComercial"
@@ -224,7 +257,181 @@ Autorização: [
   ]}
 ],
 
-  Liquidação: [],
+
+  Liquidação: [
+  { grupo: "Comuns", itens: [
+      "@Bandeira",
+      "@Bin6",
+      "@Bin8",
+      "@codigoBandeira",
+      "@codigoCredenciador",
+      "@codigoEmissor",
+      "@codigoProcessadora",
+      "@codigoRemessa",
+      "@Credenciador",
+      "@date",
+      "@dd.date",
+      "@Emissor",
+      "@finalCartao",
+      "@hostname",
+      "@mascaraCartao",
+      "@MCCLiquidacao",
+      "@ModoEntrada",
+      "@plataforma",
+      "@processo",
+      "@Produto",
+      "@remessaCredenciador",
+      "@sequencial",
+      "@service",
+      "@tipoRejeicao",
+      "@tipoTransacao"
+  ]},
+
+  { grupo: "dadosAdicionais", itens: [
+      "@dadosAdicionais.codigoRastreioCicloVidaTransacao",
+      "@dadosAdicionais.dataRejeicaoEmissor",
+      "@dadosAdicionais.flagCartaoPresente",
+      "@dadosAdicionais.indicadorRota",
+      "@dadosAdicionais.kafkaUID"
+  ]},
+
+  { grupo: "dadosLiquidacao.dadosRemessa.remessas", itens: [
+      "@dadosLiquidacao.dadosRemessa.remessas.dataReferenciaMovimento",
+      "@dadosLiquidacao.dadosRemessa.remessas.numeroRemessa",
+      "@dadosLiquidacao.dadosRemessa.remessas.origem",
+      "@dadosLiquidacao.dadosRemessa.remessas.plataforma",
+      "@dadosLiquidacao.dadosRemessa.remessas.versaoArquivoMensageria"
+  ]},
+
+  { grupo: "dadosLiquidacao.participantes", itens: [
+      "@dadosLiquidacao.participantes.codigo",
+      "@dadosLiquidacao.participantes.razaoSocial",
+      "@dadosLiquidacao.participantes.tipoParticipante"
+  ]},
+
+  { grupo: "Registro00", itens: [
+      "@registro00.bancoEmissor",
+      "@registro00.cidadePontoVenda",
+      "@registro00.codigoAutorizacaoTransacao",
+      "@registro00.codigoBandeira",
+      "@registro00.codigoCredenciador",
+      "@registro00.codigoMoedaTransacao",
+      "@registro00.codigoMotivoDisputa",
+      "@registro00.codigoPaisPontoVenda",
+      "@registro00.codigoProcesso",
+      "@registro00.codigoTransacao",
+      "@registro00.dataMovimentoOuDisputa",
+      "@registro00.dataVendaSaque",
+      "@registro00.horaVendaSaque",
+      "@registro00.identificadorTipoTransacao",
+      "@registro00.indicadorOrigemAutorizacaoCancelamento",
+      "@registro00.indicadorTecnologiaTerminal",
+      "@registro00.indicadorTipoBeneficio",
+      "@registro00.mccPontoVenda",
+      "@registro00.meioIdentificacaoPortador",
+      "@registro00.modoEntradaTransacaoPos",
+      "@registro00.nomePontoVenda",
+      "@registro00.numeroCartao",
+      "@registro00.numeroReferenciaTransacao",
+      "@registro00.subCodigoTransacao",
+      "@registro00.tipoLiquidacao",
+      "@registro00.valorVendaSaqueDisputa"
+  ]},
+
+  { grupo: "Registro01", itens: [
+      "@registro01.carteiraDigitalId",
+      "@registro01.codigoCondicionalTransacaoComChip",
+      "@registro01.codigoProduto",
+      "@registro01.codigoTransacao",
+      "@registro01.cpfCnpj",
+      "@registro01.indicadorEnvioDocumentacao",
+      "@registro01.indicadorMovimentacao",
+      "@registro01.indicadorTransacaoPorTelefoneOuEcommerce",
+      "@registro01.numeroLogicoEquipamento",
+      "@registro01.numeroParcela",
+      "@registro01.numeroReferenciaDisputa",
+      "@registro01.pontoVenda",
+      "@registro01.quantidadeParcelasTransacao",
+      "@registro01.subCodigoTransacao",
+      "@registro01.tarifaPagamentoInsumo",
+      "@registro01.textoLivreEmissorCredenciador",
+      "@registro01.tipoPessoa",
+      "@registro01.valorTaxaEmbarque",
+      "@registro01.valorTransacao",
+      "@registro01.valorTrocoOuAgroDebito"
+  ]},
+
+  { grupo: "Registro02", itens: [
+      "@registro02.cepEstabelecimentoComercial",
+      "@registro02.codigoIbge",
+      "@registro02.codigoPaisLiquidacao",
+      "@registro02.codigoPontoVendaOuMarketplace",
+      "@registro02.codigoTransacao",
+      "@registro02.dataLiquidacaoTransacao",
+      "@registro02.dataMovimentoTransacaoOriginal",
+      "@registro02.idReferenciaBandeira",
+      "@registro02.quantidadeDiasLiquidacaoFinanceiraTransacao",
+      "@registro02.subCodigoTransacao",
+      "@registro02.tipoOperacao",
+      "@registro02.tokenAssuranceLevel",
+      "@registro02.tokenPan",
+      "@registro02.tokenRequestorId",
+      "@registro02.valorIntercambio"
+  ]},
+
+  { grupo: "Registro05", itens: [
+      "@registro05.codigoMoedaValorAutorizado",
+      "@registro05.codigoQualificadorTransacao",
+      "@registro05.codigoRespostaAutorizacao",
+      "@registro05.codigoResultadoVerificacaoCavv",
+      "@registro05.codigoTransacao",
+      "@registro05.identificadorTransacao",
+      "@registro05.indicadorAutorizacaoEspecifica",
+      "@registro05.indicadorComercioEletronico",
+      "@registro05.indicadorDireitoDevolucao",
+      "@registro05.numeroSequenciaComponenteTransacao",
+      "@registro05.valorAutorizado",
+      "@registro05.valorTotalAutorizado",
+      "@registro05.valorVerificacaoAutenticacaoPortadorCavv"
+  ]},
+
+  { grupo: "Registro07", itens: [
+      "@registro07.applicationInterchangeProfile",
+      "@registro07.capacidadeTerminal",
+      "@registro07.codigoMoeda",
+      "@registro07.codigoPaisTerminal",
+      "@registro07.codigoQualificadorTransacao",
+      "@registro07.codigoTransacao",
+      "@registro07.contadorTransacaoAplicacao",
+      "@registro07.criptograma",
+      "@registro07.dadosAplicacaoEmissor",
+      "@registro07.dataTransacaoTerminal",
+      "@registro07.formFactorIndicator",
+      "@registro07.indiceDerivacaoChave",
+      "@registro07.numeroRandomicoCriptograma",
+      "@registro07.numeroSequenciaComponenteTransacao",
+      "@registro07.numeroSequencialCartao",
+      "@registro07.numeroSerieTerminal",
+      "@registro07.numeroVersaoCriptograma",
+      "@registro07.tipoTransacao",
+      "@registro07.valorSecundarioTransacao",
+      "@registro07.valorTransacaoCriptograma",
+      "@registro07.verificacaoResultadoCartao",
+      "@registro07.verificacaoResultadoTerminal"
+  ]},
+
+  { grupo: "Registro09", itens: [
+      "@registro09.codigoErro",
+      "@registro09.codigoTransacao",
+      "@registro09.codigoTransacaoOriginal",
+      "@registro09.dataMovimento",
+      "@registro09.descricaoErro",
+      "@registro09.PosicaoComErro",
+      "@registro09.registroComErro",
+      "@registro09.subCodigoTransacao"
+  ]}
+],
+
   Standin: [
 
   // =========================
